84ec72dc1f69333233563a8926efaa3b5f8d2c2a
Andrey Kaiky Reis Ferreira <Andrey.kaiky@outlook.com>
2024-08-18 16:41:08 -0300

    chore: Update frontend HTML and CSS
    This commit updates the frontend HTML and CSS code. It renames the stylesheet file to "style.css" and moves it to the "assets" folder. It also updates the color and font size of the navigation links, and adds a larger heading style. These changes improve the visual design and organization of the frontend.

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 require('dotenv').config();
 
 // Middleware para JSON
 app.use(bodyParser.json());
+
+// Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Importando as rotas
 const postsRouter = require('./routes/posts');
@@ -12,5 +16,5 @@ app.use('/posts', postsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
